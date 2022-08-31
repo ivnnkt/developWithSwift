2d4424f1f09269bebcab5d536fc8ab8789893a53
Nikita <ivnnkt@gmail.com>
2022-08-31 13:41:31 +0300

    first steps with CoreData

---
+++ WorkingWithCoreData/WorkingWithCoreData/PostEntity.swift
@@ -0,0 +1,13 @@
+//
+//  PostEntity.swift
+//  WorkingWithCoreData
+//
+//  Created by Nikita on 31.08.2022.
+//
+
+import Foundation
+import CoreData
+
+class PostEntity: NSManagedObject {
+    
+}
